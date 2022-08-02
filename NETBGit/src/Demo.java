1b65ca1a34b8df27924f70fdc4f89739cb6e3f14
ShubhangiTalekar <110378578+ShubhangiTalekar@users.noreply.github.com>
2022-08-02 09:02:49 +0200

    By Shubh

--- NETBGit/src/Demo.java
+++ NETBGit/src/Demo.java
@@ -3,9 +3,15 @@ public class Demo {
 public void m1
 {
 	System.out.println("Hello git");
+}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
 	}
+	
+	public void m2
+{
+	System.out.println("Hello ");
+}
 
 }
