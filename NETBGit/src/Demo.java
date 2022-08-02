2e34abf22b97cd0b760edfd07ec082b64c4992a8
shubh <shubh@LAPTOP-LC9207QH>
2022-08-02 10:37:55 +0200

    By shubh

--- NETBGit/src/Demo.java
+++ NETBGit/src/Demo.java
@@ -1,11 +1,26 @@
 
-public class Demo {
-public void m1
+public class Demo 
 {
-	System.out.println("Hello git");
-	public static void main(String[] args) 
+	public void m1()
 	{
-		System.out.println("Hello");
+	System.out.println("Hello");
 	}
-
+	
+	public void m2()
+	{
+	System.out.println("Hello==M2");
+	}
+	
+	public void m3()
+	{
+	System.out.println("Hello==M3");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.println("Main Method");
+	}
 }
+
