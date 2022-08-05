229ec8155b7f374f3eb215333027971e8493f1d2
shubh <shubh@LAPTOP-LC9207QH>
2022-08-05 08:46:50 +0200

    By Shubhangi

---
+++ NETBGit/src/GitDemo.java
@@ -0,0 +1,13 @@
+
+public class GitDemo {
+
+	public static void main(String[] args) 
+	{
+		    System.out.println("Hello");
+			System.out.println("Main Method");
+			System.out.println("Hello gits");
+			System.out.println("Main Method");
+		
+	}
+
+}
