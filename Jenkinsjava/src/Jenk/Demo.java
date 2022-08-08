f98896a1cca402cf6d10a4e52f6bca12351a90ac
shubh <shubh@LAPTOP-LC9207QH>
2022-08-08 07:22:35 +0200

    Jenkins

---
+++ Jenkinsjava/src/Jenk/Demo.java
@@ -0,0 +1,13 @@
+package Jenk;
+
+public class Demo 
+{
+	public static void main(String[] args) 
+		{
+			System.out.println("Hello");
+	                System.out.println("Hiiiii");
+	                System.out.println("Shahil");
+		}
+
+	
+}
